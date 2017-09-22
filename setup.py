07bae107726df8db47bc5fe164f7040fd10bbc0f
George Brighton <george@gebn.co.uk>
2017-09-23 00:07:47 +0100

    Bump version to 1.0.0-rc.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def _read_file(name, encoding='utf-8') -> str:
 
 setup(
     name='wood',
-    version='1.0.0-rc.1',
+    version='1.0.0-rc.2',
     description='Compare directories, efficiently sync changes to AWS, and '
                 'invalidate CDNs.',
     long_description=_read_file('README.rst'),
