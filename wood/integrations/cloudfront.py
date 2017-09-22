f31830efb8e0be4011e02c25858edd800a8ca618
George Brighton <george@gebn.co.uk>
2017-09-22 21:53:03 +0100

    Inject the boto3 client into the CloudFront integration, rather than implicitly creating it

--- wood/integrations/cloudfront.py
+++ wood/integrations/cloudfront.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
-from typing import Iterator
+from typing import Iterator, Any
 import logging
-
-import boto3
 
 from wood.invalidate import PrefixInvalidator
 
 
 logger = logging.getLogger(__name__)
-
-_CLIENT = boto3.client('cloudfront')
 
 
 class CloudFrontInvalidator(PrefixInvalidator):
@@ -17,14 +13,17 @@ class CloudFrontInvalidator(PrefixInvalidator):
     Invalidates paths in a CloudFront distribution.
     """
 
-    def __init__(self, distribution: str, reference: str):
+    def __init__(self, client: Any, distribution: str, reference: str):
         """
         Initialise a new CloudFront invalidator.
 
+        :param client: The boto3 CloudFront client to use when interacting with
+                       the service.
         :param distribution: The distribution ID to invalidate.
         :param reference: A description for any invalidations performed using
                           this instance.
         """
+        self._client = client
         self._distribution = distribution
         self._reference = reference
 
@@ -35,7 +34,7 @@ class CloudFrontInvalidator(PrefixInvalidator):
             return
         logger.info('Invalidating %d prefixes (%s)', len(prefixes),
                     ', '.join(prefixes))
-        response = _CLIENT.create_invalidation(
+        response = self._client.create_invalidation(
             DistributionId=self._distribution,
             InvalidationBatch={
                 'Paths': {
