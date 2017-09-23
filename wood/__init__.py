ff56718c03483ee17af234bcd57616c8f09ea667
George Brighton <george@gebn.co.uk>
2017-09-23 02:00:47 +0100

    Make module convenience functions much more flexible in what they accept

--- wood/__init__.py
+++ wood/__init__.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-import os
-
-import pathlib
 from typing import Union
-
 from pkg_resources import get_distribution, DistributionNotFound
+import os
+import pathlib
 
 from wood.entities import Root as _Root, Entity as _Entity
 
 from wood.comparison import Comparison
 from wood.integrations import cloudflare, cloudfront, s3
-root = _Root.from_path
-entity = _Entity.from_path
 
 __title__ = 'wood'
 __author__ = 'George Brighton'
@@ -22,13 +18,46 @@ __copyright__ = 'Copyright 2017 George Brighton'
 # adapted from http://stackoverflow.com/a/17638236
 try:
     dist = get_distribution(__title__)
-    path = os.path.normcase(dist.location)
+    dist_path = os.path.normcase(dist.location)
     pwd = os.path.normcase(__file__)
-    if not pwd.startswith(os.path.join(path, __title__)):
+    if not pwd.startswith(os.path.join(dist_path, __title__)):
         raise DistributionNotFound()
     __version__ = dist.version
 except DistributionNotFound:
     __version__ = 'unknown'
+
+
+def _normalise_path(path: Union[str, pathlib.Path]) -> pathlib.Path:
+    """
+    Ensures a path is parsed.
+
+    :param path: A path string or Path object.
+    :return: The path as a Path object.
+    """
+    if isinstance(path, str):
+        return pathlib.Path(path)
+    return path
+
+
+def root(path: Union[str, pathlib.Path]) -> _Root:
+    """
+    Retrieve a root directory object from a path.
+
+    :param path: The path string or Path object.
+    :return: The created root object.
+    """
+    return _Root.from_path(_normalise_path(path))
+
+
+def entity(path: Union[str, pathlib.Path]) -> _Entity:
+    """
+    Retrieve an appropriate entity object from a path.
+
+    :param path: The path of the entity to represent, either a string or Path
+                 object.
+    :return: An entity representing the input path.
+    """
+    return _Entity.from_path(_normalise_path(path))
 
 
 def compare(left: Union[str, pathlib.Path, _Entity],
@@ -53,7 +82,7 @@ def compare(left: Union[str, pathlib.Path, _Entity],
         if isinstance(param, str):
             param = pathlib.Path(param)
         if isinstance(param, pathlib.Path):
-            param = entity(param)
+            param = _Entity.from_path(param)
         return param
 
     return Comparison.compare(normalise(left), normalise(right))
